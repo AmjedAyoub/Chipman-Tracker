77a86f6e7eb918f69ee6b2ac1667ef4d2e85d0df
Amjed Ayoub <amjadkayoub@gmail.com>
2021-10-27 11:53:17 -0700

    fix DB

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(
-      "mongodb+srv://amjed:chipman-track-app@chipmantracker.yg93a.mongodb.net/techs?retryWrites=true&w=majority", {
+      "mongodb+srv://amjed:5afFn6jmKxPzq56@chipmantracker.yg93a.mongodb.net/techs?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
